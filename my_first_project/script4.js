4f92438cf173669a7642a33c13a378485214cb47
Shubh kamal <crotonaldehyde938@gmail.com>
2026-03-10 15:54:25 +0530

    Update the digital clock

--- my_first_project/script4.js
+++ my_first_project/script4.js
@@ -4,8 +4,20 @@ function updateClock(){
     let hours = now.getHours();
     let minutes=now.getMinutes();
     let seconds=now.getSeconds();
-    let milliseconds=now.getMilliseconds();
-    let time =hours+":"+minutes+":"+seconds+":"+milliseconds;
+    let ampm=hours >=12 ? "PM":"AM";
+
+    hours=hours%12;
+    hours=hours? hours:12;
+
+    hours = String(hours).padStart(2,"0");
+minutes = String(minutes).padStart(2,"0");
+seconds = String(seconds).padStart(2,"0");
+    
+    let time =hours+":"+minutes+":"+seconds;
     document.getElementById("clock").innerText=time;
+}
+function updateDay(){
+    let day= now.getDay();
+    
 }
 setInterval(updateClock,1000);
